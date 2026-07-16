53426dcbe2a6008c24fc6a224f27c1a2428c6b01
Viktoriia Sagaidachna <victoriya.sagaidachna@gmail.com>
2026-07-16 09:21:34 +0300

    HW 6

---
+++ src/hw6/App.java
@@ -0,0 +1,12 @@
+package hw6;
+
+public class App {
+    static void main() {
+        int result = 0;
+        int value = 6;
+        for (int i=1; i <= value; i++){
+         result += i;
+            System.out.println("Num is " + i + ",sum is " + result);
+        }
+    }
+}
